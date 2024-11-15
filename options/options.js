bce031c5a02a051def085e065d7053c4aa063998
Crowdbotics <team@crowdbotics.com>
2024-11-15 15:52:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-glade-50056",
+  url: "https://steep-glade-50056.botics.co",
+  api: "https://steep-glade-50056.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
